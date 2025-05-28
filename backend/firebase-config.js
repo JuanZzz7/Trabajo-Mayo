705f4d878958e8c923e9a17e07b0c4f3c8503f15
JuanZzz7 <dpena5803@gmail.com>
2025-05-28 11:29:28 -0500

    agregacion de archivo js

---
+++ backend/firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBsIQ7bwAxmidadMrBxHWh8OH62JKtIQyM",
+  authDomain: "ejemplodefirebase-372f7.firebaseapp.com",
+  databaseURL: "https://ejemplodefirebase-372f7-default-rtdb.firebaseio.com",
+  projectId: "ejemplodefirebase-372f7",
+  storageBucket: "ejemplodefirebase-372f7.firebasestorage.app",
+  messagingSenderId: "538254715539",
+  appId: "1:538254715539:web:59d49d7658ede10c3abb66"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
